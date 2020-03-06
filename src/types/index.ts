e6a6d2e2ce3c302c2b7bc3273941e4d0f2b36a3f
shengqiong <shengjiong007@outlook.com>
2020-03-06 17:27:13 +0800

    create basic axios and examples

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'Delete'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method // HTTP 方法；
+  data?: any //post、patch 等类型请求的数据，放到 request body 中的
+  params?: any //params 是 get、head 等类型请求的数据，拼接到 url 的 query string 中的。
+}
