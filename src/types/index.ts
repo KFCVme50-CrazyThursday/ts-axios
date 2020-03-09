d6241ff6fdf2db3f52314b52b26f87ce421807a1
shengqiong <shengjiong007@outlook.com>
2020-03-09 17:36:34 +0800

    headers

--- src/types/index.ts
+++ src/types/index.ts
@@ -19,4 +19,5 @@ export interface AxiosRequestConfig {
   method?: Method // HTTP 方法；
   data?: any //post、patch 等类型请求的数据，放到 request body 中的
   params?: any //params 是 get、head 等类型请求的数据，拼接到 url 的 query string 中的。
+  headers?: any
 }
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,11 +1,19 @@
 import { AxiosRequestConfig } from './types'
 
 export default function xhr(config: AxiosRequestConfig) {
-  const { data = null, url, method = 'get' } = config
+  const { data = null, url, method = 'get',headers } = config
 
   const request = new XMLHttpRequest()
 
   request.open(method.toUpperCase(), url, true)
+
+  Object.keys(headers).forEach(name => {
+    if (data === null && name.toLowerCase() === 'content-type') {
+      delete headers[name]  //传入的 data 为空的时候，请求 header 配置 Content-Type 是没有意义的
+    } else {
+      request.setRequestHeader(name, headers[name])
+    }
+  })
 
   request.send(data)
 }
