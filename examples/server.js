40e13c3f907d6b47e6ec445bdb1f79d895e9e2d7
shengqiong <shengjiong007@outlook.com>
2020-03-09 15:56:07 +0800

    处理url参数

--- examples/server.js
+++ examples/server.js
@@ -27,10 +27,14 @@ app.use(bodyParser.urlencoded({
 
 const router = express.Router()
 
-router.get('/simple/get', function(req, res) {
+router.get('/simple/get', function (req, res) {
   res.json({
     msg: `hello world`
   })
+})
+
+router.get('/base/get', function (req, res) {
+  res.json(req.query)
 })
 
 app.use(router)
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import { buildURL } from './helpers/url'
 
 function axios(config: AxiosRequestConfig): void {
   // TODO
+  processConfig(config)
   xhr(config)
 }
+
+function processConfig (config: AxiosRequestConfig): void {
+  config.url = transformUrl(config)
+}
+
+function transformUrl (config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
+}
+
 export default axios
